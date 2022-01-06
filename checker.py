a77beedb7ac4d41650b67f192152ac3af017d983
ricegrains <52660210+r1cegrains@users.noreply.github.com>
2022-01-06 16:18:48 +1100

    Update checker.py

--- checker.py
+++ checker.py
@@ -2,7 +2,7 @@ import requests
 import time
 
 def versionLoop():
-    oFile = open("", "r")
+    oFile = open("", "r") # Opens the file to read
     
     oldVersion = f'{oFile.read()}'
     newVersion = requests.get('') # Grabs the version from Roblox
@@ -10,9 +10,9 @@ def versionLoop():
     if newVersion.text in oldVersion:
         print("no updates")
     else:
-        cFile = open("version.txt", "w")
+        cFile = open("version.txt", "w") # Opens the file to write the new version
         cFile.write(newVersion.text)
-        cFile.close()
+        cFile.close() # Closes the file
         print('Updated version in files')
         print(f'New Roblox version: {newVersion.text}')
 
